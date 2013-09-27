1ea91e56b4895021e3cade419cc99188eac922d8
Johannes Alneberg <alneberg@kth.se>
2013-09-27 22:05:10 +0200

    Elin's page

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = Flask(__name__)
 def main():
     return render_template('index.html')
 
+@app.route('/elin')
+def elin():
+    return render_template('elin.html')
+
 if __name__ == '__main__':
     app.run()
